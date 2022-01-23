6d408b091a3b2e291ddb3aa3ca8f5405dc3282f2
DaymianDark <nightkonn@gmail.com>
2022-01-23 16:48:42 +0300

    any change 3

--- src/test/java/com/berezkindv/simple/NegativeTests.java
+++ src/test/java/com/berezkindv/simple/NegativeTests.java
@@ -2,27 +2,27 @@ package com.berezkindv.simple;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NegativeTests {
     @Test
     void test00() {
-        assertFalse(false);
+        assertTrue(false);
     }
 
     @Test
     void test01() {
-        assertFalse(false);
+        assertTrue(false);
     }
 
     @Test
     void test02() {
-        assertFalse(false);
+        assertTrue(false);
     }
 
     @Test
     void test03() {
-        assertFalse(false);
+        assertTrue(false);
     }
 
 }
