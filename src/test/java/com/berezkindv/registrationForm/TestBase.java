22cfc9f2d629cf1fa39e630a37bf3564fdb5d936
DaymianDark <nightkonn@gmail.com>
2022-01-24 13:15:03 +0300

    added steps

--- src/test/java/com/berezkindv/registrationForm/TestBase.java
+++ src/test/java/com/berezkindv/registrationForm/TestBase.java
@@ -1,13 +1,19 @@
 package com.berezkindv.registrationForm;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
+    @Step("Конфигурируем браузер и удаленный запуск")
     static void beforeAllMethod() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
 
