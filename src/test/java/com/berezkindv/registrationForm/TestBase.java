abca97aa96ba83a7bc10bf1927b82f7ecfab11f2
DaymianDark <nightkonn@gmail.com>
2022-01-24 10:34:53 +0300

    added selenoid support

--- src/test/java/com/berezkindv/registrationForm/TestBase.java
+++ src/test/java/com/berezkindv/registrationForm/TestBase.java
@@ -2,11 +2,19 @@ package com.berezkindv.registrationForm;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAllMethod() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
