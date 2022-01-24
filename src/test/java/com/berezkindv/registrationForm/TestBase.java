7548ad01df746c12099ba68a6ed456ae573d455b
DaymianDark <nightkonn@gmail.com>
2022-01-24 13:34:17 +0300

    added attachments

--- src/test/java/com/berezkindv/registrationForm/TestBase.java
+++ src/test/java/com/berezkindv/registrationForm/TestBase.java
@@ -1,9 +1,11 @@
 package com.berezkindv.registrationForm;
 
+import com.berezkindv.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 //import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Step;
 //import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -16,11 +18,19 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
